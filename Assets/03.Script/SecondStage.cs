137fce3179291628c6dd11c21e939ef0b5ea649c
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-06-24 23:52:52 +0900

    2stage edit

--- Assets/03.Script/SecondStage.cs
+++ Assets/03.Script/SecondStage.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System;
 
 public class SecondStage : ReFirstStage
 {
     public GameObject powerEffect;
+    public GameObject powerMap;
+
     void Start()
     {
         Song.Stop();
@@ -48,13 +51,26 @@ public class SecondStage : ReFirstStage
         }
         if (allNotes <= 0)
         {
-            ClearPanel.SetActive(true);
+            StartCoroutine(Clear(5f));
         }
-        if (allNotes <= maxNotes - 10)
+        if (allNotes == maxNotes - 112)
         {
-            powerEffect.SetActive(true);
+            powerMap.SetActive(true);
+            map.MapSpeed *= 3;
         }
 
+        if (allNotes == maxNotes - 154)
+        {
+            StartCoroutine(EffectTrue(0.45f, powerEffect));
+        }
+        if (allNotes == maxNotes - 155)
+        {
+            map.MapSpeed *= 3;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log(maxNotes - allNotes);
+        }
         double beatInterval = 60d / bpm;
 
         currentTime += Time.deltaTime;
