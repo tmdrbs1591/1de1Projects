a9c90a78fb7180208f77b263d1fd5a866f091275
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-06-26 14:24:37 +0900

    Editor Fix

--- Assets/03.Script/ThirdStage.cs
+++ Assets/03.Script/ThirdStage.cs
@@ -13,25 +13,12 @@ public class ThirdStage : ReFirstStage
         theEffectManager = FindObjectOfType<EffectManager>();
         theTimingManager = GetComponent<TimingManager>();
 
-#if UNITY_EDITOR
-        if (DataManager.instance.songPath == null) // 메인메뉴에서 고른게 없으면
+        string path = Application.dataPath + "/Songs/" + DataManager.instance.songPath + ".json";
+        using (StreamReader reader = new StreamReader(path))
         {
-            // 창을 띄워서 할당하게 파일을
-            var path = EditorUtility.OpenFilePanel("Load wave", Application.dataPath, "json");
-            using (StreamReader reader = new StreamReader(path))
-            {
-                notemap = JsonUtility.FromJson<EditorManager.SerializableList<NoteInfo>>(reader.ReadToEnd()).list;
-            }
+            notemap = JsonUtility.FromJson<EditorManager.SerializableList<NoteInfo>>(reader.ReadToEnd()).list;
         }
-        else //그게 아니면
-        {
-            //빌드랑 공유된 리소스 파일에서 가져오기
-            var jsonString = (TextAsset)Resources.Load(DataManager.instance.songPath);
-            notemap = JsonUtility.FromJson<EditorManager.SerializableList<NoteInfo>>(jsonString.text).list;
-        }
-#else
-        Debug.LogError("Song path can only be set in the Unity Editor.");
-#endif
+
 
         foreach (NoteInfo e in notemap)
         {
